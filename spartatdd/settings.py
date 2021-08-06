272d5b42bd3eac943bb3bff5e7b9fe260efedd86
Hyun - Woo <nikel4610@gmail.com>
2021-08-06 12:42:14 +0900

    Update settings.py

--- spartatdd/settings.py
+++ spartatdd/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e-#5zn!n65y211^1kvi37v6yem1b2sul*blog$jftr0giuin0!'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
